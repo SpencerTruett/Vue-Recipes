0e86d19b330acfe0de5763de8591f69a04a49f0b
Spencer Truett <srtruett1993@gmail.com>
2021-03-29 10:18:02 -0500

    created Recipe component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <v-app>
     <v-btn @click="getRandomRecipes">Find recipes</v-btn>
+    <RecipeResults />
   </v-app>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import RecipeResults from "./components/RecipeResults"
 
 export default {
+  components: { RecipeResults },
   methods: {
     ...mapActions(["getRandomRecipes"])
   }
