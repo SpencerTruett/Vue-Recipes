92f1db1907fb6fe698c317d5c11d6fd9e9d2dfcb
Spencer Truett <srtruett1993@gmail.com>
2021-03-29 11:52:22 -0500

    cleaned up main template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <v-app>
-    <KeyIngredients />
-    <DietSelect />
-    <MealTypeSelect />
-    <v-btn @click="getRandomRecipes">Find recipes</v-btn>
-    <RecipeResults />
+    <v-main class="container mt-10">
+      <h1 class="text-center primary--text mb-4">Recipe Assistant</h1>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <key-ingredients />
+          </v-col>
+          <v-col cols="12" md="4">
+            <diet-select />
+          </v-col>
+          <v-col cols="12" md="4">
+            <meal-type-select />
+          </v-col>
+        </v-row>
+        <v-row class="justify-center mt-10">
+          <v-btn color="primary" @click="getRandomRecipes">Find recipes</v-btn>
+        </v-row>
+        <recipe-results />
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
